994b2712d7aeea9573e04c4999f3569f93e15a0c
kikipet <songkimwl@gmail.com>
2021-03-11 18:35:29 -0500

    init

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from "react";
+import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  count: {
+    fontSize: 48,
+  },
+})
+
+class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+  }
+
+  inc = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.inc, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Text style={styles.count}>{this.state.count}</Text>
+      </View>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCounter: true
+    };
+  }
+
+  toggleCounter = () => this.setState(prevState => ({
+    showCounter: !prevState.showCounter
+  }))
+
+  render() {
+    if (this.state.showCounter) {
+      return (
+        <View style={styles.appContainer}>
+          <Button title="toggle" onPress={this.toggleCounter} />
+          <Counter />
+        </View>
+      );
+    }
+    else {
+      return <Button title="toggle" onPress={this.toggleCounter} />
+    }
+  }
+}
